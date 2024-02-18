13827975fac2dc1a88d8ee4fa6ae54896911be9a
uuuuuno <mail@uuuu.uno>
2024-02-18 18:05:45 +0300

    fix delete server script

--- delete-server.sh
+++ delete-server.sh
@@ -4,17 +4,10 @@ read -p "Enter server name (without special characters or spaces): " servername
 NFS_SERVER_IP=$(cat .env | grep NFS_SERVER_IP | cut -d '=' -f 2)
 LOCAL_MOUNT_DIR=$(cat .env | grep LOCAL_MOUNT_DIR | cut -d '=' -f 2)
 
+kubectl delete -f manifests/mc-${servername}-manifest.yaml
+
 sudo mount -t nfs ${NFS_SERVER_IP}:/home/mc ${LOCAL_MOUNT_DIR}
-rm -rf ${LOCAL_MOUNT_DIR}/${servername}
+rm -fr ${LOCAL_MOUNT_DIR}/${servername}
 rm manifests/mc-${servername}-manifest.yaml
 
 echo "Done."
-
-read -p "Delete the local manifest? (y/n)? " delete_manifest
-
-if [[ $delete_manifest == "Y" || $delete_manifest == "y" ]]; then
-    kubectl delete -f mc-${servername}-manifest.yaml
-    echo "The manifest was successfully deleted."
-else
-    echo "The manifest has not been deleted."
-fi
